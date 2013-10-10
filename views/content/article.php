5fa97317cf2b7b59d945ea1024fa8076e3f83682
Yordan Arnaudov <y_arnaudov@yahoo.com>
2013-10-10 09:36:48 +0300

    article view

--- views/content/article.php
+++ views/content/article.php
@@ -7,16 +7,18 @@
     <div class="sights_full" >
 	
 		<?php if(isset($article['params']['images'][0]) && is_array($article['params']['images'][0])){ ?>
-		<img src="<?=$this->Image->getImageUrl($article['params']['images'][0]['id'], 150, 110);?>" >
+		<img src="<?=$this->Image->getImageUrl($article['params']['images'][0]['id'], 150, 110);?>" class="first_image" >
 		<?php }elseif(isset($article['params']['images'][0])){ ?>
 		<img src="<?=base_url($article['params']['images'][0]);?>" >
 		<?php } ?>
 	
         <?=$article['text'];?>
 		
+		
 		<?php 
 		
-		if(isset($article['params']['images'])){		
+		if(isset($article['params']['images'])){	
+			echo "<div class=\"sights_images\" >\n";
 			foreach($article['params']['images'] as $image){
 			
 				if(is_array($image)){
@@ -27,9 +29,11 @@
 				}				
 			
 			}		
+			echo "</div>\n";
 		}
 		
 		?>
+		
 		
     </div>
    
